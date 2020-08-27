f8568aebd880e7df17ef50659178b714cd5391d4
Tas <tas0173@gmail.com>
2020-08-27 10:27:55 +1000

    Commit with starting of code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Console.log the UFO data from data.js
+console.log(tableData);
+
+// Create a reference to the table body
+var tbody = d3.select("tbody");
+
+// Create a reference to the filter button (Exercise 3/06)
+var filterButton = d3.select(".filter-btn")
+ 
+// Create a reference to the required columns 
+
+
+
+
+
+
+
