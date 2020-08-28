3cb3f2cca7c30f78450b7eaf4e4e199cba10b9ec
Tas <tas0173@gmail.com>
2020-08-28 11:10:16 +1000

    Updates - added in table building For Loop

--- UFO-level-1/static/js/apptest.js
+++ UFO-level-1/static/js/apptest.js
@@ -1,48 +1,58 @@
-// Assign the data from `data.js` to a descriptive variable
-var tableData = data;
-var tbody = d3.select("tbody")
+// Assign the data from `data.js` to a descriptive variable (use const here)
+const tableData = data;
+const tbody = d3.select("tbody")
 
 // Select the button
 var filterButton = d3.select("#filter-btn");
 
-// Create a d3 reference to the datetime input
-var inputDate = d3.select("#datetime");
 
-// Create a reference to the required columns
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+// build a function here as the table is only bing built once - need to be able to call over and over again
+data.forEach((ufoOutput) => {
+  var row = tbody.append("tr");
+  Object.entries(ufoOutput).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
 
 // Create event handlers 
 filterButton.on("click", runEnter);
-
 
 // Complete the event handler function for the form
 function runEnter() {
   
   // Prevent the page from refreshing
   d3.event.preventDefault();
-  
-  // Select the input element and get the raw HTML node
-  var inputElement = inputDate.select("input");
+
+  // Create a d3 reference to the datetime input
+  var inputDate = d3.select("#datetime");
   
   // Get the value property of the input element
-  var userInput = inputElement.property("value");  
+  var userInput = inputDate.property("value");  
   
   // Use the form input to filter the data by date of UFO sighting
   var matchingResults = tableData.filter(ufoSighting => ufoSighting.datetime == userInput);
+
+  console.log(matchingResults);
   
   // Get the element for printing out the results
   var ufoElement = d3.select("tr");
-  
+
+  // populate here 
+  data.forEach((filteredUfoOutput) => {
+    var rows = matchingResults.append("tr");
+    Object.entries(matchingResults).forEach(([key, value]) => {
+      var cells = rows.append("td");
+      cells.text(value);
+    });
+  });
+
   // Handle no matching results
   if (matchingResults.length == 0) {
     ufoElement.html("");
     ufoElement.append("td").text(`There were no UFO's on the ${userInput}`);
     return;
   }
-
- //  Add the output to the table using the "tr" tag 
-  ufoElement.html("");
-  ufoElement.append("td").text(columns);
 
 console.log(ufoElement);
   
